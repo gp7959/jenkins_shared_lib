b0cd77fed3b160c3b6ac43c5ac8b95e463d83ce6
gp7959 <gp7959@gmail.com>
2023-09-05 22:16:22 +0530

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,8 +1,8 @@
 def call(credentialsId){
     bat 'java -version'
     def sonarToken = credentials(credentialsId)
-    echo "SonarQube Token: ${sonarToken}"
+    echo "SonarQube Token: $sonarToken"
     withSonarQubeEnv(credentialsId: credentialsId){
-        bat "mvn sonar:sonar -Dsonar.token=${sonarToken}"
+        bat "mvn sonar:sonar -Dsonar.token=$sonarToken"
     }
 }
