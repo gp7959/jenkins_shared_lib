5132cf6670005e8ad3211a7910b33921378489da
gp7959 <gp7959@gmail.com>
2023-09-06 17:15:00 +0530

    Update deployToCloudhub.groovy

--- vars/deployToCloudhub.groovy
+++ vars/deployToCloudhub.groovy
@@ -1,3 +1,3 @@
-def call(map cloudParams){
+def call(Map cloudParams){
     bat 'mvn deploy -DmuleDeploy -Pcloudhub1 -DappName=${cloudParams.appName} -DdeployEnv=${cloudParams.deployEnv} -Dregion=${cloudParams.region} -Dworkers=${cloudParams.workers} -DworkerType=${cloudParams.workerType} -DplatformClientId=${cloudParams.platformClientId} -DplatformClientSecret=${cloudParams.platformClientSecret}'
 }
